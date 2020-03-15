215360618d31d775ecfd68c74ca9885b9176a540
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-15 18:31:47 -0500

    headline component tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Header from './components/header/index';
+import Header from 'components/header/index';
+import Headline from 'components/headline/index';
 import './app.scss';
 function App() {
   return (
     <div className='App'>
       <Header />
+      <section className='main'>
+        <Headline header='headline' desc='eat more carrots' />
+      </section>
     </div>
   );
 }
