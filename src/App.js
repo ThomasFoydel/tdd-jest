be8d423ea1698847c046c3857fff2fcc80d5700e
ThomasFoydel <thomasjfoydel@gmail.com>
2020-03-15 22:22:39 -0500

    proptypes tests

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Header from 'components/header/index';
 import Headline from 'components/headline/index';
 import './app.scss';
+
+const tempArr = [
+  {
+    fName: 'Joe',
+    lName: 'Mariot',
+    email: 'joemariot@gmail.com',
+    age: 24,
+    onlineStatus: true
+  }
+];
+
 function App() {
   return (
     <div className='App'>
       <Header />
       <section className='main'>
-        <Headline header='headline' desc='eat more carrots' />
+        <Headline header='title' desc='eat more carrots' tempArray={tempArr} />
       </section>
     </div>
   );
